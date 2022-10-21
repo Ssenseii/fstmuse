a3f6e390348603a661eddb94094dc5646b7c4a65
Simon Reinhardt <saadabou.study@gmail.com>
2022-10-21 19:04:52 +0100

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Art from "./pages/Art";
+import Library from "./pages/Library";
+import NoPage from "./pages/NoPage";
 
 const App = () => {
   return (
-    <div>
-
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="art" element={<Art />} />
+          <Route path="library" element={<Library />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+
+
+
+function Navbar() {
+
+
+
+  return (
+    <nav className="nav">
+      <button className="nav__toggleMenu">
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+
+      <ul className={`nav__menu`}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/art">Art</Link>
+        </li>
+        <li>
+          <Link to="/library">Library</Link>
+        </li>
+      </ul>
+
+      <div className="nav__logo">
+        <h3>FSTMUSE</h3>
+      </div>
+
+      <div className="nav__toggleTheme">
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="nav__toggleTheme-icon"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
+            />
+          </svg>
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
